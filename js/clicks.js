8d31ca4c6a51166440a6ff20d976ca7b363d81e8
Casey Schneebeck <cschneebeck@tnc.org>
2016-09-28 13:21:54 -0500

    initial build

--- js/clicks.js
+++ js/clicks.js
@@ -5,233 +5,66 @@ function ( Query, QueryTask, declare, FeatureLayer, lang, on, $, ui, esriapi ) {
         "use strict";
 
         return declare(null, {
-			chosenListeners: function(t){
-				// Enable jquery plugin 'chosen'
-				require(["jquery", "plugins/coastline-change/js/chosen.jquery"],lang.hitch(this,function($) {
-					var configCrs =  { '.chosen-islands' : {allow_single_deselect:false, width:"186px", disable_search:true}}
-					for (var selector in configCrs)  { $(selector).chosen(configCrs[selector]); }
+			clickListener: function(t){
+				// Handle Class changes on all togBtn clicks
+				$('#' + t.id + ' .togBtn').on('click', lang.hitch(t,function(c){		
+					// Go to parent of selected button, find all elements with class togBtn, and remove togBtnSel from each one
+					$.each($(c.currentTarget).parent().find('.togBtn'), lang.hitch(t,function(i, x){
+						$(x).removeClass('togBtnSel');
+					}))
 				}));
-				// User selections on chosen menus 
-				require(["jquery", "plugins/coastline-change/js/chosen.jquery"],lang.hitch(t,function($) {	
-				t.esriapi = new esriapi();
-					//Select CRS 
-					$('#' + t.id + 'ch-ISL').chosen().change(lang.hitch(t,function(c, p){
-						t.obj.islSelected = c.currentTarget.value;
-						if(c.target.value != 'EasternShore'){
-							t.obj.islandText = c.target.value + " Island";
-							if(t.obj.termSelected == 'long'){
-								$('#' + t.id + 'chartTitle').text(t.obj.islandText + ' – Long Term Change')
-							}
-							if(t.obj.termSelected == 'short'){
-								$('#' + t.id + 'chartTitle').text(t.obj.islandText + ' – Short Term Change')
-							}
-							// create new query and and select features
-							var q = new Query();
-							q.where = "IslandName = '" + t.obj.islSelected + "'";
-							t.islFeat.selectFeatures(q,FeatureLayer.SELECTION_NEW);
-							// Hide selected island polygon
-							var q1 = new Query();
-							q1.where = "IslandName <> '" + t.obj.islSelected + "'";
-							t.islandPolygons.selectFeatures(q1,FeatureLayer.SELECTION_NEW);
-							
-							t.islandPolygons_click.setDefinitionExpression("IslandName <> '" + t.obj.islSelected + "'");
-							// Hide selcted island label
-							var layerDefinitions = [];
-							layerDefinitions[0] = q1.where;
-							layerDefinitions[3] = "Island = '" + t.obj.islSelected + "'";
-							layerDefinitions[4] = "Island = '" + t.obj.islSelected + "'";
-							t.dynamicLayer.setLayerDefinitions(layerDefinitions);
-							// check to see if in historical 
-							if(t.obj.dataTypeButton == 'hisShoreBtn'){
-								t.obj.visibleLayers = [0];
-								$('#' + t.id + 'ch-yearCheck .yearCb').each(lang.hitch(t,function(i, v){
-									if(v.checked == true){
-										var val = v.value;
-										$.each(t.layersArray, lang.hitch(t,function(i,v){
-											var layerName = v.name;
-											if(layerName.split("_")[1] == val){
-												t.obj.yearLayerID = v.id;
-												t.obj.visibleLayers.push(t.obj.yearLayerID);
-												t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-											}
-										}));
-										t.obj.visibleLayers = []
-									}
-								}));
-							}
-							if(t.obj.termSelected == "long" && t.obj.dataTypeButton == 'chRateBtn'){
-								t.obj.historicLayers = [0,4];
-								t.obj.visibleLayers = t.obj.historicLayers;
-								t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-							}
-							if(t.obj.termSelected == "short" && t.obj.dataTypeButton == 'chRateBtn'){
-								t.obj.historicLayers = [0,3];
-								t.obj.visibleLayers = t.obj.historicLayers;
-								t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-							}							
-							//t.obj.zoomedIn = "yes";
-						}else{
-							if(t.obj.termSelected == 'long'){
-								$('#' + t.id + 'chartTitle').text('Virginia Eastern Shore – Long Term Change')
-							}
-							if(t.obj.termSelected == 'short'){
-								$('#' + t.id + 'chartTitle').text('Virginia Eastern Shore – Short Term Change')
-							}
-							// Show all island labels
-							var layerDefinitions = [];
-							layerDefinitions[0] = "IslandName <> 'a'";
-							t.dynamicLayer.setLayerDefinitions(layerDefinitions);
-							t.islandPolygons_click.setDefinitionExpression("IslandName <> 'xyz'");
-							var q1 = new Query();
-							q1.where = "IslandName <> 'xyz'";
-							t.islandPolygons.selectFeatures(q1,FeatureLayer.SELECTION_NEW);
-							// zoom to eastern shore
-							t.map.setExtent(t.obj.initialExtent, true);
-							//t.obj.zoomedIn = "no";
-							t.esriapi.esriStartUp(t);
-						}
-						
-						
-						if(t.obj.trigger != 'mapClick'){
-							var query = new esri.tasks.Query();
-							query.where = "IslandName = '" + t.obj.islSelected + "'"
-							t.islandPolygons_click.selectFeatures(query,esri.layers.FeatureLayer.SELECTION_NEW);
-							t.obj.trigger = 'dropDown'
-						}
-					}));
+				// Update visibility on togBtn clicks
+				$('#' + t.id + ' .togBtnWrap .togBtn').on('click', lang.hitch(t,function(c){	
+					$(c.currentTarget).addClass('togBtnSel');
+					t.clicks.layerDefUpdate(t);
 				}));
-				// HANDLE INDIVIDUAL SHORELINE CHECKBOX CLICKS AND CHECK ALL YEAR SHORELINES///////////////////////////////////////////////////////////////////////////////////
-				$('#' + t.id + 'ch-yearCheck .yearCbWrap').on('click',lang.hitch(this,function(c){
+				// checkbox clicks
+				$('#' + t.id + 'cbListener .cbWrap').on('click',lang.hitch(this,function(c){
 					// if they click a label toggle the checkbox
 					if (c.target.checked == undefined){
 						$(c.currentTarget.children[0]).prop("checked", !$(c.currentTarget.children[0]).prop("checked") )	
 					}
-					t.obj.checkYearArray = [0];
-					t.obj.checkedMultiYears = [];
-					$('#' + t.id + 'ch-yearCheck .yearCb').each(lang.hitch(t,function(i, v){
-						if(v.checked == true){
-							var lyr = v.value + " Shoreline";
-							$.each(t.layersArray, lang.hitch(t,function(j,w){
-								if (lyr == w.name){
-									t.obj.checkYearArray.push(w.id)
-									t.obj.checkedMultiYears.push("ch-" + v.value);
-								}	
-							}));	
-						}
-					}));
-					t.obj.historicLayers = t.obj.checkYearArray;
-					t.obj.visibleLayers = t.obj.historicLayers;
-					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+					if ($(c.currentTarget.children[0]).prop('checked') === true){
+						$(c.currentTarget).parent().find('.togBtnWrap').slideDown();
+					}else{
+						$(c.currentTarget).parent().find('.togBtnWrap').slideUp();
+						$.each($(c.currentTarget).parent().find('.togBtn'), lang.hitch(t,function(i, x){
+							$(x).removeClass('togBtnSel');
+						}))
+						t.clicks.layerDefUpdate(t);
+					}	
 				}));
-				// SET UP SLIDER AND ADD EVENT LISTENER //////////////////
-				var vals = 13;
-				var labels = ['1850s','1910s','1940s', '1962', '1970s', '1985-6', '1994', '2002', '2004', '2006', '2009', '2011', '2013', '2014'];
-				for (var i = 0; i <= vals; i++) {
-					var el = $('<label>'+(labels[i])+'</label>').css('left',(i/vals*100)+'%');
-					$('#' + t.id + 'multiShoreSlider').append(el);
-				}
-				$('#' + t.id + 'multiShoreSlider').on('slide', lang.hitch(this,function(w,evt){
-					t.obj.sliderCounter = evt.value;
-					$.each(t.layersArray, lang.hitch(t,function(i,v){
-						if ( t.obj.sliderCounter == v.id){
-							t.obj.yearLayerID = v.id;
-							t.obj.historicLayers = [t.obj.yearLayerID];
-							t.obj.visibleLayers = t.obj.historicLayers;
-							t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+			},
+			layerDefUpdate: function(t){
+				var expArray = [];
+				$.each($('#' + t.id + ' .togBtnWrap .togBtn'), lang.hitch(t,function(i, v){
+					if ($(v).hasClass('togBtnSel')){
+						var lngId = $(v).prop('id').substr($(v).prop('id').indexOf("-") + 1);
+						var field = lngId.substr(0, lngId.indexOf('-'));
+						var val = lngId.slice(-1);
+						expArray.push(field + " = " + val ); 
+					}	
+				}));
+				var exp = "OBJECTID < 0";
+				if (expArray.length > 0){
+					$.each(expArray, lang.hitch(t,function(i,v){
+						if (i == 0){
+							exp = v;	
+						}else{
+							exp = exp + " AND " + v;
 						}	
-					}));
-					if (t.sliderPlayBtn  == 'play'){
-						t.sliderPlayBtn  == ''
-						$('#' + t.id + 'sliderStop').trigger('click');
-					}
-				}));
-				// slider play button click
-				$('#' + t.id + 'sliderPlay').on('click', lang.hitch(t,function(){
-					$('#' + t.id + 'sliderPlay').addClass('hide');
-					$('#' + t.id + 'sliderStop').removeClass('hide');
-					t.sliderPlayBtn  = 'play' 
-					t.setInt = setInterval(function(){
-						$('#' + t.id + 'multiShoreSlider').slider('value',t.obj.sliderCounter);
-						var x  = t.obj.sliderCounter;
-						t.obj.historicLayers = [0, x];
-						t.obj.visibleLayers = t.obj.historicLayers;
-						t.obj.yearLayerID = t.obj.sliderCounter;
-						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-						t.obj.sliderCounter++
-						if(t.obj.sliderCounter>20){
-							t.obj.sliderCounter = 7
-						}
-						
-					}, 1000);
-				}));
-				//slider stop button click
-				$('#' + t.id + 'sliderStop').on('click', lang.hitch(t,function(){
-					$('#' + t.id + 'sliderPlay').removeClass('hide');
-					$('#' + t.id + 'sliderStop').addClass('hide');
-					clearInterval(t.setInt);
-					t.sliderPlayBtn  = '';
-				}));
-				//FUTURE CLICKS ////////////////////
-				//inlet arrays
-				t.inlet1 = ['Future Scenarios: High - Greater NE - None','Future Scenarios: Highest - Greater NE - None',
-							'Future Scenarios: High - Greater NE - Wallops', 'Future Scenarios: High - Greater NE - Assateague',
-							'Future Scenarios: High - Greater NE - Both', 'Future Scenarios: Highest - Greater NE - Wallops',
-							'Future Scenarios: Highest - Greater NE - Assateague', 'Future Scenarios: Highest - Greater NE - Both'];
-				t.inlet2 = ['Future Scenarios: High - Lesser NE - Both', 'Future Scenarios: Highest - Lesser NE - Both'];
-				// click on any of the buttons in the future wrapper section.
-				$('#' + t.id + 'futButtonWrapper .togBtn').on('click', lang.hitch(t,function(c){
-					t.obj.visibleLayers = [];
-					var nm = c.target.innerText;		
-					// Add conditional logic based on id of parent div
-					var pid = $('#' + c.currentTarget.id).parent().parent().prop('id');
-					if (pid == t.id + "riseWrapper"){
-						t.obj.seaLevelValue = nm;
-						t.obj.riseBtn = "-" + c.target.id.substr(c.target.id.indexOf("-") + 1);
-					}
-					if (pid == t.id + "waveWrapper"){
-						t.obj.waveValue = " - " + nm;
-						t.obj.waveBtn = "-" + c.target.id.substr(c.target.id.indexOf("-") + 1);
-					}
-					if (pid == t.id + "nourWrapper"){
-						t.obj.nourValue = " - " + nm;
-						t.obj.nourBtn = "-" + c.target.id.substr(c.target.id.indexOf("-") + 1);						
-					}
-					t.obj.fsLyrName = "Future Scenarios: " + t.obj.seaLevelValue + t.obj.waveValue + t.obj.nourValue;
-					if (t.obj.nourValue == ' - Wallops'){
-						t.obj.visibleLayers.push(24)
-					}
-					if (t.obj.nourValue == ' - Assateague'){
-						t.obj.visibleLayers.push(23)
-					}
-					if (t.obj.nourValue == ' - Both'){
-						t.obj.visibleLayers.push(23)
-						t.obj.visibleLayers.push(24)
-					}
-					$.each(t.inlet1, lang.hitch(t,function(i,v){
-						if 	(t.obj.fsLyrName == v){
-							t.obj.visibleLayers.push(21)	
-						}	
-					}))
-					$.each(t.inlet2, lang.hitch(t,function(i,v){
-						if 	(t.obj.fsLyrName == v){
-							t.obj.visibleLayers.push(22)	
-						}	
-					}))					
-					$.each(t.layersArray, lang.hitch(t,function(i,v){
-						if (t.obj.fsLyrName == v.name){
-							t.obj.visibleLayers.push(v.id);
-							t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-							$.each(t.futurePercents, lang.hitch(t,function(j,w){
-								if (w.cleanName == v.name){
-									var a = JSON.parse(w.percentArray);
-									a.reverse();
-									t.barChart.updateChart(a, t)
-								}	
-							}));							
-						}	
-					}));	
-				}));
-			}
+					}))	
+				}	
+				var layerDefinitions = [];
+				layerDefinitions[0] = exp;
+				t.dynamicLayer.setLayerDefinitions(layerDefinitions);
+				var query = new Query();
+				var queryTask = new QueryTask(t.url + '/0');
+				query.where = exp;
+				queryTask.executeForCount(query,function(count){
+					$('#' + t.id + 'basinCnt').html(count); 
+				});
+			}	
         });
     }
 );
--- main.js
+++ main.js
@@ -5,32 +5,32 @@ require({
 // Bring in dojo and javascript api classes as well as varObject.json, js files, and content.html
 define([
 	"dojo/_base/declare", "framework/PluginBase", "dijit/layout/ContentPane", "dojo/dom", "dojo/dom-style", "dojo/dom-geometry", "dojo/_base/lang", "dojo/text!./obj.json", 
-	"jquery", "dojo/text!./html/content.html", './js/jquery-ui-1.11.2/jquery-ui', './js/navigation', './js/esriapi', './js/clicks', './js/stateCh', './js/barChart'
+	"jquery", "dojo/text!./html/content.html", './js/jquery-ui-1.11.2/jquery-ui', './js/navigation', './js/esriapi', './js/clicks', './js/barChart'
 ],
 function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj, 
-			$, content, ui, navigation, esriapi, clicks, stateCh, barChart ) {
+			$, content, ui, navigation, esriapi, clicks, barChart ) {
 	return declare(PluginBase, {
 		// The height and width are set here when an infographic is defined. When the user click Continue it rebuilds the app window with whatever you put in.
-		toolbarName: "Coastline Change", showServiceLayersInLegend: true, allowIdentifyWhenActive: false, rendered: false, resizable: false,
-		hasCustomPrint: true, usePrintPreviewMap: true, previewMapSize: [1000, 550], height:"600", width:"430",
-		infoGraphic: 'plugins/coastline-change/assets/coastline_change_main_infographic.png',
+		toolbarName: "Benefits Explorer", showServiceLayersInLegend: true, allowIdentifyWhenActive: false, rendered: false, resizable: false,
+		hasCustomPrint: true, usePrintPreviewMap: true, previewMapSize: [1000, 550], height:"450", width:"360",
+		
 		// First function called when the user clicks the pluging icon. 
 		initialize: function (frameworkParameters) {
 			// Access framework parameters
 			declare.safeMixin(this, frameworkParameters);
 			// Set initial app size based on split screen state
-			this.con = dom.byId('plugins/coastline-change-0');
-			this.con1 = dom.byId('plugins/coastline-change-1');
+			this.con = dom.byId('plugins/benefits_explorer-0');
+			this.con1 = dom.byId('plugins/benefits_explorer-1');
 			if (this.con1 != undefined){
-				domStyle.set(this.con1, "width", "430px");
-				domStyle.set(this.con1, "height", "600px");
+				domStyle.set(this.con1, "width", "360px");
+				domStyle.set(this.con1, "height", "450px");
 			}else{
-				domStyle.set(this.con, "width", "430px");
-				domStyle.set(this.con, "height", "600px");
+				domStyle.set(this.con, "width", "360px");
+				domStyle.set(this.con, "height", "450px");
 			}	
 			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
 			this.obj = dojo.eval("[" + obj + "]")[0];	
-			this.url = "http://dev.services2.coastalresilience.org:6080/arcgis/rest/services/Virginia/va_coastline_change/MapServer";
+			this.url = "http://dev.services2.coastalresilience.org:6080/arcgis/rest/services/Water_Blueprint/BenefitsExplorer/MapServer";
 			this.layerDefs = [];
 		},
 		// Called after initialize at plugin startup (why all the tests for undefined). Also called after deactivate when user closes app by clicking X. 
@@ -50,11 +50,9 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 				this.dynamicLayer.setVisibility(true);
 			}else{
 				this.map.addLayer(this.dynamicLayer);
-				this.map.addLayer(this.islandPolygons);
-				this.map.addLayer(this.islandPolygons_click);
 				// on set state it calls activate twice. on the second call render is true so it call this else. layer infos isn't done yet so if you call setNavBtns it can't use layer infos
 				if (this.obj.stateSet == "no"){	
-					this.navigation.setNavBtns(this);	
+					//this.navigation.setNavBtns(this);	
 				}else{
 					this.obj.stateSet = "no";
 				}	
@@ -62,14 +60,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 		},
 		// Called when user hits the minimize '_' icon on the pluging. Also called before hibernate when users closes app by clicking 'X'.
 		deactivate: function () {
-			this.map.showZoomSlider();
-			this.map.enableDoubleClickZoom();
-			this.map.enableRubberBandZoom();
-			this.map.enableScrollWheelZoom();
-			this.map.removeLayer(this.dynamicLayer);
-			this.map.removeLayer(this.islandPolygons);
-			this.map.removeLayer(this.islandPolygons_click);	
-			$('.basemap-selector').show();			
+				
 		},	
 		// Called when user hits 'Save and Share' button. This creates the url that builds the app at a given state using JSON. 
 		// Write anything to you varObject.json file you have tracked during user activity.		
@@ -109,7 +100,6 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 			this.navigation = new navigation();
 			this.esriapi = new esriapi();
 			this.clicks = new clicks();
-			this.stateCh = new stateCh();
 			// ADD HTML TO APP
 			// Define Content Pane as HTML parent		
 			this.appDiv = new ContentPane({style:'padding:8px 8px 8px 8px; position:relative;'});
@@ -121,20 +111,13 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 			
 			//create slider bar
 			$('#' + this.id + 'multiShoreSlider').slider({ min: 7,	max: 20, value: 7, step: 1 });
-			
-			this.obj.initialExtent = this.map.extent;
-			// SET UP BAR CHART
-			this.barChart.makeChart(this);
+			// Click listeners
+			this.clicks.clickListener(this);
 			// CALL NAVIGATION BUTTON EVENT LISTENERS 
 			this.navigation.navListeners(this);
-			//Call the function to populate the initial graph
-			this.esriapi.esriStartUp(this);
 			// CREATE ESRI OBJECTS AND EVENT LISTENERS	
 			this.esriapi.esriApiFunctions(this);
-			// CREATE CHOSEN SELECT MENUS AND EVENT LISTENERS	
-			this.clicks.chosenListeners(this);				
-			// UPDATE STATE IF SET STATE WAS CALLED
-			//this.stateCh.checkState(this);
+			
 			this.rendered = true;	
 			// resize the container in the render function after the container is built.
 			this.resize1();
