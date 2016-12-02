f9cba6276bca3fd71a2c6a20fba7a7eb4b321cc0
Casey Schneebeck <cschneebeck@tnc.org>
2016-12-02 13:47:16 -0600

    update

---
+++ js/clicks.js
@@ -0,0 +1,154 @@
+define([
+	"esri/tasks/query", "esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", "dojo/_base/lang", "dojo/on", "jquery", './jquery-ui-1.11.2/jquery-ui', './esriapi'
+],
+function ( Query, QueryTask, declare, FeatureLayer, lang, on, $, ui, esriapi ) {
+        "use strict";
+
+        return declare(null, {
+			clickListener: function(t){
+				// Infographic section clicks
+				$('.plugin-infographic .be_accordHeader').on('click',lang.hitch(t,function(c){
+					if ( $(c.currentTarget).next().is(":hidden") ){
+						$('.plugin-infographic .be_exWrap').slideUp();
+						$(c.currentTarget).next().slideDown();
+					}	
+				}));
+				$('#' + t.id + ' .be_minfo').on('click',lang.hitch(t,function(c){
+					var ben = c.target.id.split("-").pop();
+					$('.plugin-help').trigger('click');
+					$('.plugin-infographic .' + ben).trigger('click');
+					$('.plugin-infographic .be_infoWrap').siblings('span').children().html('Back');
+				}));	
+				// Hide/show benefit sections
+				$('#' + t.id + ' .be_hs').on('click',lang.hitch(t,function(c){
+					if ( $(c.currentTarget).next().is(":hidden") ){
+						$('#' + t.id + ' .be_sectionWrap').slideUp();
+						$(c.currentTarget).next().slideDown();
+					}	
+				}));
+				// Explain benefits click
+				$('#' + t.id + 'moreInfo').on('click',lang.hitch(t,function(c){
+					$('#' + t.id + 'moreInfo span').toggle();
+					$('#' + t.id + ' .explanations').slideToggle();
+				}));	
+				// Benefit CB Clicks
+				$('#' + t.id + 'cbListener .be_cbBenWrap').on('click',lang.hitch(t,function(c){
+					var ben = "";
+					// if they click a label toggle the checkbox
+					if (c.target.checked == undefined){
+						$(c.currentTarget.children[0].children[0]).prop("checked", !$(c.currentTarget.children[0].children[0]).prop("checked") )	
+						ben = $(c.currentTarget.children[0].children[0]).val()
+					}else{
+						ben = c.target.value;
+					}	
+					if ($(c.currentTarget.children[0].children[0]).prop('checked') === true){
+						$(c.currentTarget).parent().find('.be_rangeWrap').slideDown();
+						var values = $('#' + t.id + '-' + ben).slider("option", "values");
+						$('#' + t.id + '-' + ben).slider('values', values); 
+					}else{
+						$(c.currentTarget).parent().find('.be_rangeWrap').slideUp();
+						t[ben] = "";
+						t.clicks.layerDefsUpdate(t);
+						$('#' + t.id + ben + '-range').html("")
+						$('#' + t.id + ben + '-unit').hide();
+					}	
+				}));	
+				// Standing Carbon range slider
+				$('#' + t.id + '-standingc').slider({range:true, min:0, max:9600, values:[0,9600], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
+				// Forest Loss range slider
+				$('#' + t.id + '-forloss').slider({range:true, min:0, max:20000, values:[0,20000], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
+				// Reforestation Potential range slider
+				$('#' + t.id + '-refor').slider({range:true, min:0, max:65100, values:[0,65100], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
+				// Freshwater Biodiversity Threats range slider
+				$('#' + t.id + '-freshbiot').slider({range:true, min:0, max:10, values:[0,10], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
+				// IUCN Listed Terrestrial Species range slider
+				$('#' + t.id + '-terrsp').slider({range:true, min:0, max:220, values:[0,220], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
+				// Habitat for Pollinators and Impacts on Vitamin A range slider
+				$('#' + t.id + '-vita' ).slider({ range: true, min: 0, max: 85, values: [ 0, 85 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
+				// Habitat for Pollinators and Impacts on Crop Yield Economic Output range slider
+				$('#' + t.id + '-agloss' ).slider({ range: true, min: 0, max: 70, values: [ 0, 70 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
+				// Excess Nitrogen range slider
+				$('#' + t.id + '-nitrogen' ).slider({ range: true, min: 0, max: 615, values: [ 0, 615 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
+			},
+			sliderChange: function( event, ui, t ){
+				var ben  = event.target.id.split("-").pop()
+				t[ben] = "(" + ben + " >= " + ui.values[0] + " AND " + ben + " <= " + ui.values[1] + ")";	
+				t.clicks.layerDefsUpdate(t);
+				var low = 0;
+				var high = 0;
+				if (ben == 'freshbiot'){
+					low = ui.values[0]/10;
+					high = ui.values[1]/10;			
+				}else{	
+					low = t.clicks.numberWithCommas(ui.values[0])
+					high = t.clicks.numberWithCommas(ui.values[1])
+				}
+				if (low == high){						
+					$('#' + t.id + ben + '-range').html("(" + low);
+				}else{
+					$('#' + t.id + ben + '-range').html("(" + low + " - " + high);
+				}
+				$('#' + t.id + ben + '-unit').show();
+			},	
+			layerDefsUpdate: function(t){
+				t.exp = [t.standingc, t.forloss, t.refor, t.freshbiot, t.terrsp, t.vita, t.agloss, t.nitrogen]
+				var exp = "";
+				var cnt = 0;
+				var nd = "f";
+				$.each(t.exp, lang.hitch(t,function(i, v){
+					if (v.length > 0){
+						if (exp.length == 0){
+							exp = v;
+							cnt = 1;
+						}else{
+							exp = exp + " AND " + v;
+							cnt = cnt + 1;
+						}	
+					}	
+				}));
+				if (cnt == 1){
+					$('#' + t.id + 'cbListener input').each(function(i,v){
+						if ($(v).prop('checked')){
+							t.exp1 = $(v).val() + " = -99";
+						}	
+					});
+					var q = new Query();
+					var qt = new QueryTask(t.url + '/0');
+					q.where = t.exp1;
+					qt.executeForCount(q,function(count){
+						var layerDefinitions = [];
+						layerDefinitions[1] = exp;
+						if (count > 0){
+							layerDefinitions[0] = t.exp1;	
+							t.obj.visibleLayers = [0,1,2];
+						}else{
+							t.obj.visibleLayers = [1,2];
+						}
+						t.dynamicLayer.setLayerDefinitions(layerDefinitions);
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);						
+					});
+				}else{	
+					if (exp.length == 0){
+						exp = "OBJECTID < 0";
+						t.obj.visibleLayers = [2];
+					}else{
+						t.obj.visibleLayers = [1,2];
+					}		
+					var layerDefinitions = [];		
+					layerDefinitions[1] = exp;	
+					t.dynamicLayer.setLayerDefinitions(layerDefinitions);
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				}
+				var query = new Query();
+				var queryTask = new QueryTask(t.url + '/0');
+				query.where = exp;
+				queryTask.executeForCount(query,function(count){
+					$('#' + t.id + 'basinCnt').html(count); 
+				});
+			},
+			numberWithCommas: function(x){
+				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+			}			
+        });
+    }
+);
---
+++ main.js
@@ -0,0 +1,123 @@
+// Pull in your favorite version of jquery 
+require({ 
+	packages: [{ name: "jquery", location: "http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/", main: "jquery.min" }] 
+});
+// Bring in dojo and javascript api classes as well as varObject.json, js files, and content.html
+define([
+	"dojo/_base/declare", "framework/PluginBase", "dijit/layout/ContentPane", "dojo/dom", "dojo/dom-style", "dojo/dom-geometry", "dojo/_base/lang", "dojo/text!./obj.json", 
+	"jquery", "dojo/text!./html/content.html", "dojo/text!./html/infoGraphic.html", './js/jquery-ui-1.11.2/jquery-ui', './js/esriapi', './js/clicks', './js/barChart', './js/horizontalBar'
+],
+function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj, 
+			$, content, ig, ui, esriapi, clicks, barChart, hbar ) {
+	return declare(PluginBase, {
+		// The height and width are set here when an infographic is defined. When the user click Continue it rebuilds the app window with whatever you put in.
+		toolbarName: "Benefits Explorer", showServiceLayersInLegend: true, allowIdentifyWhenActive: false, rendered: false, resizable: false,
+		hasCustomPrint: true, usePrintPreviewMap: true, previewMapSize: [1000, 550], height:"560", width:"390", infoGraphic: ig,		
+		// First function called when the user clicks the pluging icon. 
+		initialize: function (frameworkParameters) {
+			// Access framework parameters
+			declare.safeMixin(this, frameworkParameters);
+			// Set initial app size based on split screen state
+			this.con = dom.byId('plugins/benefits_explorer-0');
+			this.con1 = dom.byId('plugins/benefits_explorer-1');
+			if (this.con1 != undefined){
+				domStyle.set(this.con1, "width", "390px");
+				domStyle.set(this.con1, "height", "560px");
+			}else{
+				domStyle.set(this.con, "width", "390px");
+				domStyle.set(this.con, "height", "560px");
+			}	
+			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
+			this.obj = dojo.eval("[" + obj + "]")[0];	
+			this.url = "http://dev.services2.coastalresilience.org:6080/arcgis/rest/services/Water_Blueprint/BenefitsExplorer/MapServer";
+			this.layerDefs = [];
+		},
+		// Called after initialize at plugin startup (why all the tests for undefined). Also called after deactivate when user closes app by clicking X. 
+		hibernate: function () {
+			if (this.appDiv != undefined){
+				this.map.removeLayer(this.dynamicLayer);
+				this.map.removeLayer(this.basinFl);
+				this.map.graphics.clear();
+				$('.plugin-infographic .be_infoWrap').siblings('span').children().html('Next')
+			}
+		},
+		// Called after hibernate at app startup. Calls the render function which builds the plugins elements and functions.   
+		activate: function () {
+			if (this.rendered == false) {
+				this.rendered = true;							
+				this.render();
+				// Hide the print button until a hex has been selected
+				$(this.printButton).hide();
+				this.dynamicLayer.setVisibility(true);
+			}else{
+				this.map.addLayer(this.dynamicLayer);
+				this.map.addLayer(this.basinFl);
+				// on set state it calls activate twice. on the second call render is true so it call this else. layer infos isn't done yet so if you call setNavBtns it can't use layer infos
+				if (this.obj.stateSet == "no"){	
+					//this.navigation.setNavBtns(this);	
+				}else{
+					this.obj.stateSet = "no";
+				}	
+			}		
+		},
+		// Called when user hits the minimize '_' icon on the pluging. Also called before hibernate when users closes app by clicking 'X'.
+		deactivate: function () {
+				
+		},	
+		// Called when user hits 'Save and Share' button. This creates the url that builds the app at a given state using JSON. 
+		// Write anything to you varObject.json file you have tracked during user activity.		
+		getState: function () {
+			this.obj.extent = this.map.geographicExtent;
+			this.obj.stateSet = "yes";	
+			var state = new Object();
+			state = this.obj;
+			return state;	
+		},
+		// Called before activate only when plugin is started from a getState url. 
+		//It's overwrites the default JSON definfed in initialize with the saved stae JSON.
+		setState: function (state) {
+			this.obj = state;
+		},
+		// Called when the user hits the print icon
+		beforePrint: function(printDeferred, $printArea, mapObject) {
+			printDeferred.resolve();
+		},	
+		// Resizes the plugin after a manual or programmatic plugin resize so the button pane on the bottom stays on the bottom.
+		// Tweak the numbers subtracted in the if and else statements to alter the size if it's not looking good.
+		resize1: function(w, h) {
+			cdg = domGeom.position(this.container);
+			if (cdg.h == 0) { this.sph = this.height - 40; }
+			else { this.sph = cdg.h - 32; }
+			// test
+			/*if (cdg.h == 0) { this.sph = this.height - 80; }
+			else { this.sph = cdg.h - 62; }*/
+			domStyle.set(this.appDiv.domNode, "height", this.sph + "px"); 
+		},
+		// Called by activate and builds the plugins elements and functions
+		render: function() {
+			
+			$('.basemap-selector').trigger('change', 3);
+			// BRING IN OTHER JS FILES
+			this.barChart = new barChart();
+			this.hbar = new hbar();
+			this.esriapi = new esriapi();
+			this.clicks = new clicks();
+			// ADD HTML TO APP
+			// Define Content Pane as HTML parent		
+			this.appDiv = new ContentPane({style:'padding:0px 8px 8px 8px; position:relative;'});
+			this.id = this.appDiv.id
+			dom.byId(this.container).appendChild(this.appDiv.domNode);					
+			// Get html from content.html, prepend appDiv.id to html element id's, and add to appDiv
+			var idUpdate = content.replace(/id='/g, "id='" + this.id);	
+			$('#' + this.id).html(idUpdate);
+			// Click listeners
+			this.clicks.clickListener(this);
+			// CREATE ESRI OBJECTS AND EVENT LISTENERS	
+			this.esriapi.esriApiFunctions(this);
+			
+			this.rendered = true;	
+			// resize the container in the render function after the container is built.
+			this.resize1();
+		},
+	});
+});
