2922303191f67f2b2762fb212c59ecf90aaa8830
Casey Schneebeck <cschneebeck@tnc.org>
2017-04-14 14:50:46 -0600

    first build

--- js/clicks.js
+++ js/clicks.js
@@ -1,226 +1,115 @@
 define([
-	"esri/tasks/query", "esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", "dojo/_base/lang", "dojo/on", "jquery", './jquery-ui-1.11.2/jquery-ui', './esriapi', "dojo/dom",
+	"dojo/_base/declare", "esri/tasks/query", "esri/tasks/QueryTask", "esri/layers/FeatureLayer", "esri/dijit/Search", "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color"
 ],
-function ( Query, QueryTask, declare, FeatureLayer, lang, on, $, ui, esriapi, dom ) {
+function ( declare, Query, QueryTask,FeatureLayer, Search, SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color) {
         "use strict";
 
         return declare(null, {
-			clickListener: function(t){
-				//make accrodians
-				$( function() {
-					$( "#" + t.id + "mainAccord" ).accordion({heightStyle: "fill"}); 
-					$( "#" + t.id + "infoAccord" ).accordion({heightStyle: "fill"});
-				});
-				// update accordians on window resize
-				var doit;
-				$(window).resize(function(){
-					clearTimeout(doit);
-					doit = setTimeout(function() {
-						t.clicks.updateAccord(t);
-					}, 100);
-				});
-				// leave the get help section
-				$('#' + t.id + 'getHelpBtn').on('click',lang.hitch(t,function(c){
-					if ( $('#' + t.id + 'mainAccord').is(":visible") ){
-						$('#' + t.id + 'infoAccord').show();
-						$('#' + t.id + 'mainAccord').hide();
-						$('#' + t.id + 'getHelpBtn').html('Back to Benefits Explorer');
-						t.clicks.updateAccord(t);
-						$('#' + t.id + 'infoAccord .infoDoc').trigger('click');
-					}else{
-						$('#' + t.id + 'infoAccord').hide();
-						$('#' + t.id + 'mainAccord').show();
-						$('#' + t.id + 'getHelpBtn').html('Back to Documentation');
-						t.clicks.updateAccord(t);
-					}					
-				}));
+			eventListeners: function(t){
+				// layer ids
+				t.techLyr = 1;
+				t.Numberofshorelineedgetechniquesthatareapplicable 
+				// leave help button
+				$('#' + t.id + 'getHelpBtn').on('click', function(c){
+					$('#' + t.id + ' .aus-wrap').show()
+					$('#' + t.id + ' .aus-help').hide()
+				})
 				// info icon clicks
-				$('#' + t.id + ' .sty_infoIcon').on('click',lang.hitch(t,function(c){
-					$('#' + t.id + 'mainAccord').hide();
-					$('#' + t.id + 'infoAccord').show();
-					$('#' + t.id + 'getHelpBtnWrap').show();
+				$('#' + t.id + ' .infoIcon').on('click',function(c){
+					t.showHelp();
 					var ben = c.target.id.split("-").pop();
+					$('#' + t.id + 'getHelpBtn').html('Back to aus Floodplain Explorer');
 					t.clicks.updateAccord(t);	
 					$('#' + t.id + 'infoAccord .' + ben).trigger('click');
-					$('#' + t.id + 'getHelpBtn').html('Back to Benefits Explorer');
-				}));		
-				// Benefit CB Clicks
-				$('#' + t.id + 'basinByBensWrap .sty_cbWrap').on('click',lang.hitch(t,function(c){
-					var ben = "";
-					// if they click a label toggle the checkbox
-					if (c.target.checked == undefined){
-						$(c.currentTarget.children[0].children[0]).prop("checked", !$(c.currentTarget.children[0].children[0]).prop("checked") )	
-						ben = $(c.currentTarget.children[0].children[0]).val()
-					}else{
-						ben = c.target.value;
-					}	
-					if ($(c.currentTarget.children[0].children[0]).prop('checked') === true){
-						$(c.currentTarget).parent().find('.sty_rangeWrap').slideDown();
-						var values = $('#' + t.id + '-' + ben).slider("option", "values");
-						$('#' + t.id + '-' + ben).slider('values', values); 
-					}else{
-						$(c.currentTarget).parent().find('.sty_rangeWrap').slideUp();
-						t[ben] = "";
-						t.clicks.layerDefsUpdate(t);
-						$('#' + t.id + ben + '-range').html("")
-						$('#' + t.id + ben + '-unit').hide();
-					}	
-				}));	
-				// Sup data CB Clicks
-				$('#' + t.id + 'supDataWrap .sty_cbStackedWrap').on('click',lang.hitch(t,function(c){
-					var val = "";
-					// if they click a label toggle the checkbox
-					if (c.target.checked == undefined){
-						$(c.currentTarget.children[0].children[0]).prop("checked", !$(c.currentTarget.children[0].children[0]).prop("checked") )	
-						val = $(c.currentTarget.children[0].children[0]).val()
-					}else{
-						val = c.target.value;
-					}	
-					t.obj.addDatalyr = Number( val.split("-").pop() )
-					$('#' + t.id + 'supDataWrap .sty_cb').each(lang.hitch(t,function(i,v){
-						if ( v.value != val ){
-							$(v).prop('checked', false)
-							var rl = Number( v.value.split("-").pop() )
-							var index = t.obj.visibleLayers.indexOf(rl);
-							if (index > -1) {
-								t.obj.visibleLayers.splice(index, 1);
-							}
-						}
-					}));
-					if ($(c.currentTarget.children[0].children[0]).prop('checked') === true){
-						t.obj.visibleLayers.push(t.obj.addDatalyr);
-					}else{
-						var index = t.obj.visibleLayers.indexOf(t.obj.addDatalyr);
-						if (index > -1) {
-							t.obj.visibleLayers.splice(index, 1);
-						}
-						t.obj.addDatalyr = -1;
-					}	
-					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-				}));	
-				// Standing Carbon range slider
-				$('#' + t.id + '-standingc').slider({range:true, min:0, max:6600, values:[0,6600], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
-				// Forest Loss range slider
-				$('#' + t.id + '-forloss').slider({range:true, min:0, max:20000, values:[0,20000], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
-				// Reforestation Potential range slider
-				$('#' + t.id + '-refor').slider({range:true, min:0, max:65100, values:[0,65100], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
-				// Freshwater Biodiversity Threats range slider
-				$('#' + t.id + '-freshbiot').slider({range:true, min:0, max:10, values:[0,10], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
-				// IUCN Listed Terrestrial Species range slider
-				$('#' + t.id + '-terrsp').slider({range:true, min:0, max:220, values:[0,220], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
-				// Habitat for Pollinators and Impacts on Vitamin A range slider
-				$('#' + t.id + '-vita' ).slider({ range: true, min: 0, max: 85, values: [ 0, 85 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
-				// Habitat for Pollinators and Impacts on Crop Yield Economic Output range slider
-				$('#' + t.id + '-agloss' ).slider({ range: true, min: 0, max: 70, values: [ 0, 70 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});
-				// Excess Nitrogen range slider
-				$('#' + t.id + '-nitrogen' ).slider({ range: true, min: 0, max: 615, values: [ 0, 615 ], change:function(event,ui){t.clicks.sliderChange(event,ui,t)}});	
-			},
-			sliderChange: function( event, ui, t ){
-				var ben  = event.target.id.split("-").pop()
-				t[ben] = "(" + ben + " >= " + ui.values[0] + " AND " + ben + " <= " + ui.values[1] + ")";	
-				t.clicks.layerDefsUpdate(t);
-				var low = 0;
-				var high = 0;
-				if (ben == 'freshbiot'){
-					low = ui.values[0]/10;
-					high = ui.values[1]/10;			
-				}else{	
-					low = t.clicks.numberWithCommas(ui.values[0])
-					high = t.clicks.numberWithCommas(ui.values[1])
-				}
-				if (low == high){						
-					$('#' + t.id + ben + '-range').html("(" + low);
-				}else{
-					$('#' + t.id + ben + '-range').html("(" + low + " - " + high);
-				}
-				$('#' + t.id + ben + '-unit').css('display', 'inline-block');
-			},	
-			layerDefsUpdate: function(t){
-				if (t.obj.stateSet == "no"){
-					t.obj.exp = [t.standingc, t.forloss, t.refor, t.freshbiot, t.terrsp, t.vita, t.agloss, t.nitrogen]
-				}
-				var exp = "";
-				var cnt = 0;
-				var nd = "f";
-				$.each(t.obj.exp, lang.hitch(t,function(i, v){
-					if (v.length > 0){
-						if (exp.length == 0){
-							exp = v;
-							cnt = 1;
-						}else{
-							exp = exp + " AND " + v;
-							cnt = cnt + 1;
-						}	
-					}	
-				}));
-				if (cnt == 1){
-					$('#' + t.id + 'basinByBensWrap input').each(function(i,v){
-						if ($(v).prop('checked')){
-							t.exp1 = $(v).val() + " = -99";
-						}	
-					});
-					var q = new Query();
-					var qt = new QueryTask(t.url + '/' + t.hbNoData);
-					q.where = t.exp1;
-					qt.executeForCount(q,function(count){
-						t.layerDefinitions = [];
-						t.layerDefinitions[t.hbFil] = exp;
-						t.layerDefinitions[t.selHb] = t.obj.selHbDef;
-						if (count > 0){
-							t.layerDefinitions[t.hbNoData] = t.exp1;	
-							t.obj.visibleLayers = [t.hbNoData, t.hbFil, t.hbSwh];
-						}else{
-							t.obj.visibleLayers = [t.hbFil, t.hbSwh];
-						}
-						if (t.obj.selHbDef.length > 0){
-							t.obj.visibleLayers.push(t.selHb)
-						}
-						if (t.obj.addDatalyr > -1){
-							t.obj.visibleLayers.push(t.obj.addDatalyr)
-						}	
-						t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
-						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);						
-					}); 
-				}else{	
-					if (exp.length == 0){
-						exp = "OBJECTID < 0";
-						t.obj.visibleLayers = [t.hbSwh];
-					}else{
-						t.obj.visibleLayers = [t.hbFil, t.hbSwh];
-					}		
-					if (t.obj.selHbDef.length > 0){
-						t.obj.visibleLayers.push(t.selHb)
-					}
-					if (t.obj.addDatalyr > -1){
-						t.obj.visibleLayers.push(t.obj.addDatalyr)
-					}
-					t.layerDefinitions = [];		
-					t.layerDefinitions[t.hbFil] = exp;
-					t.layerDefinitions[t.selHb] = t.obj.selHbDef;					
-					t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
-					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-				}
-				var query = new Query();
-				var queryTask = new QueryTask(t.url + '/' + t.hbNoData);
-				query.where = exp;
-				queryTask.executeForCount(query,function(count){
-					var cnt = t.clicks.numberWithCommas(count)
-					$('#' + t.id + 'basinCnt').html(cnt); 
 				});
+				// suppress help on startup click
+				$('#' + t.id + '-shosu').on('click',function(c){
+					if (c.clicked == true){
+						t.app.suppressHelpOnStartup(true);
+					}else{
+						t.app.suppressHelpOnStartup(false);
+					}
+				})
+
+// work with Radio buttons (how would you like to view shoreline data) ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				$('#' + t.id + 'aus-viewRadioWrap input').on('click',function(c){
+					var val = c.target.value
+					if (val == 'all'){
+						t.obj.visibleLayers = [0]
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						$('#' + t.id + 'aus-enhanceFuncWrap').slideUp()
+					}
+					if (val == 'ind'){
+						t.obj.visibleLayers = [t.techLyr]
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						$('#' + t.id + 'aus-enhanceFuncWrap').slideDown()
+					}
+				})
+// work with Radio buttons indiv. techniques ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				$('#' + t.id + 'funcWrapper input').on('click',function(c){
+					var val = c.target.value
+					$.each($(t.layersArray),function(i,v){
+						var lyrName = v.name;
+						if(val == lyrName){
+							t.techLyr = v.id
+							console.log(t.techLyr);
+							t.obj.visibleLayers = [v.id];
+					 		t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						}
+					});
+				})
+// work with Radio buttons sup data section ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				$('.aus-supDataWrap input').on('click',function(c){
+					var val = c.target.value
+					$.each($(t.layersArray),function(i,v){
+						var lyrName = v.name;
+						if(val == lyrName){
+							t.obj.visibleLayers = [v.id];
+					 		t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						}
+						if(val == 'None'){
+							console.log('none', t.techLyr);
+							t.obj.visibleLayers = [t.techLyr];
+					 		t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						}
+					});
+				});
+// feature layer init ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 				t.attributeData = new FeatureLayer(t.url + "/5", { mode: FeatureLayer.MODE_SELECTION, outFields: ["*"] });
+// 				// Map click ////////////////////////////////////////
+// 				t.map.on("click", function(evt) {
+// 					console.log(evt);
+// 					t.obj.pnt = evt.mapPoint;
+// 					var q = new Query();
+// 					q.geometry = t.obj.pnt;
+// 					t.attributeData.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
+				
+// 				});
+// // On selection complete ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 				t.attributeData.on('selection-complete', function(evt){
+// 					console.log(evt);
+					
+// 				});
 			},
-			updateAccord: function(t){
-				var ma = $( "#" + t.id + "mainAccord" ).accordion( "option", "active" );
-				var ia = $( "#" + t.id + "infoAccord" ).accordion( "option", "active" );
-				$( "#" + t.id + "mainAccord" ).accordion('destroy');	
-				$( "#" + t.id +  "infoAccord" ).accordion('destroy');	
-				$( "#" + t.id + "mainAccord" ).accordion({heightStyle: "fill"}); 
-				$( "#" + t.id + "infoAccord" ).accordion({heightStyle: "fill"});	
-				$( "#" + t.id + "mainAccord" ).accordion( "option", "active", ma );		
-				$( "#" + t.id + "infoAccord" ).accordion( "option", "active", ia );				
+			
+			commaSeparateNumber: function(val){
+				while (/(\d+)(\d{3})/.test(val.toString())){
+					val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+				}
+				return val;
 			},
-			numberWithCommas: function(x){
-				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-			}			
+			abbreviateNumber: function(num) {
+			    if (num >= 1000000000) {
+			        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+			     }
+			     if (num >= 1000000) {
+			        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+			     }
+			     if (num >= 1000) {
+			        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+			     }
+			     return num;
+			}
         });
     }
-)
+);
--- js/esriapi.js
+++ js/esriapi.js
@@ -1,138 +1,81 @@
 define([
 	"esri/layers/ArcGISDynamicMapServiceLayer", "esri/geometry/Extent", "esri/SpatialReference", "esri/tasks/query" ,"esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", 
-	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", "esri/graphic", "dojo/_base/Color", "dojo/_base/lang", "dojo/on", "jquery", './jquery-ui-1.11.2/jquery-ui'
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color"
 ],
 function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, QueryTask, declare, FeatureLayer, 
-			SimpleLineSymbol, SimpleFillSymbol, Graphic, Color, lang, on, $, ui) {
+			SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color ) {
         "use strict";
 
         return declare(null, {
 			esriApiFunctions: function(t){	
-				// Define dynamic layer numbers and layer def array
-				t.selHb = "0";
-				t.fwRare = "1";
-				t.terRate = "2";
-				t.avExt = "3";
-				t.bioTgt = "4";
-				t.hbNoData = "5";
-				t.hbFil = "6";
-				t.hbSwh = "7";
-				t.layerDefinitions = [];
 				// Add dynamic map service
-				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.8});
+				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.7});
 				t.map.addLayer(t.dynamicLayer);
 				if (t.obj.visibleLayers.length > 0){	
 					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
 				}
-				t.dynamicLayer.on("load", lang.hitch(t, function () { 			
+		
+
+// Dynamic layer on load ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				t.dynamicLayer.on("load", function () { 			
 					t.layersArray = t.dynamicLayer.layerInfos;
-					// Start with empty expressions
-					t.standingc = ""; 
-					t.forloss = "";
-					t.refor = "";
-					t.freshbiot = "";
-					t.terrsp = "";
-					t.vita = "";
-					t.agloss = "";
-					t.nitrogen = "";
-					t.map.setMapCursor("pointer");
-					// Save and Share 
-					if (t.obj.stateSet == "yes"){
-						//extent
-						var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
-						t.map.setExtent(extent, true);
-						// accordion visibility
-						$('#' + t.id + t.obj.accordVisible).show();
-						$('#' + t.id + t.obj.accordHidden).hide();
-						$('#' + t.id + 'getHelpBtn').html(t.obj.buttonText);
-						t.clicks.updateAccord(t);
-						$('#' + t.id + t.obj.accordVisible).accordion( "option", "active", t.obj.accordActive );
-						// hydrobasin click
-						if (t.obj.selHbDef.length > 0){
-							var q = new Query();
-							q.where = t.obj.selHbDef;
-							t.basinFl.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
-						}
-						// benefit checkboxes and sliders
-						$.each(t.obj.checkedBenefits,lang.hitch(t,function(i,v){
-							$('#' + t.id + 'basinByBensWrap input').each(lang.hitch(t,function(j,w){
-								if ( v[0] == $(w).val() ){
-									$('#' + t.id + '-' + v[0]).slider('values', v[1]);
-									$(w).trigger('click')
-								}
-							}))	
-						}))
-						// additional data checkboxes
-						if (t.obj.addDatalyr > -1){
-							$('#' + this.id + 'supDataWrap input').each(lang.hitch(this,function(i,v){
-								if( $(v).val() == "lyr-" + t.obj.addDatalyr ){
-									$(v).prop('checked', true)
-								}
-							}));
-						}
-						t.obj.stateSet = "no";
-					}else{
-						t.clicks.layerDefsUpdate(t);
+					// trigger initial top control clicks
+					$.each($('#' + t.id + 'aus-viewRadioWrap input'),function(i,v){
+						if (t.obj.viewRadCBCh == v.value){
+							console.log('look here');
+							console.log(t.obj.viewRadCBCh, v.value, v.id);
+							$('#' + v.id).trigger('click');	
+						}	
+					});
+					// trigger clicks on techniques
+					$.each($('#' + t.id + 'funcWrapper input'),function(i,v){
+						if (t.obj.enhanceTech == v.value){
+							console.log('look here');
+							console.log(t.obj.enhanceTech, v.value, v.id);
+							$('#' + v.id).trigger('click');	
+						}	
+					});
+					// trigger clicks on sup data radio buttons
+					$.each($('#' + t.id + 'aus-supDataWrap input'),function(i,v){
+						if (t.obj.supData == v.value){
+							console.log('look here');
+							console.log(t.obj.enhanceTech, v.value, v.id);
+							$('#' + v.id).trigger('click');	
+						}	
+					});
+
+					var extent = new Extent(144.1, -38.6, 145.7, -37.6, new SpatialReference({ wkid:4326 }))
+					if (t.obj.stateSet == "no"){
+						console.log('look here for set extent');
+						// t.map.setExtent(t.dynamicLayer.fullExtent.expand(1.2), true)
+						t.map.setExtent(extent, true)
 					}
-				}));	
-				var sym = new SimpleFillSymbol( SimpleFillSymbol.STYLE_SOLID, new SimpleLineSymbol(
-					SimpleLineSymbol.STYLE_SOLID, new Color([0,0,255]), 2 ), new Color([0,0,0,0.1])
-				);
-				t.basinFl = new FeatureLayer(t.url + "/" + t.hbFil, { mode: FeatureLayer.MODE_SELECTION, outFields: ["*"] });
-				t.basinFl.setSelectionSymbol(sym);
-				t.basinFl.on('selection-complete', lang.hitch(t,function(evt){
-					var index = t.obj.visibleLayers.indexOf(t.selHb);
-					if (evt.features.length > 0){
-						if ( $('#' + t.id + 'mainAccord').is(':hidden') ){
-							$('#' + t.id + 'getHelpBtn').trigger('click');
-						}
-						$('#' + t.id + 'hydroHeader').html('Selected HydroBASIN Attributes');
-						var atts = evt.features[0].attributes;
-						t.obj.selHbDef = "OBJECTID = " + atts.OBJECTID;
-						t.layerDefinitions[t.selHb] = t.obj.selHbDef;
-						t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
-						if (index == -1) {
-							t.obj.visibleLayers.push(t.selHb);
-							t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-						}
-						var b = [['standingc',atts.standingc,6568.95], ['forloss',atts.forloss,19429.33], ['refor',atts.refor,65038.4], ['freshbiot',atts.freshbiot/10,1], 
-								 ['terrsp',atts.terrsp,219], ['vita',atts.vita,84.06], ['agloss',atts.agloss,68], ['nitrogen',atts.nitrogen,611.6]];
-						t.hbar.updateHbar(t,b);
-						
-						if ($('#' + t.id + 'basinByBensWrap').is(":visible")){
-							$('#' + t.id + 'hbbHeader').trigger('click');
-						};
-						if ($('#' + t.id + 'supDataWrap').is(":visible")){
-							$('#' + t.id + 'sdWrap').trigger('click');
-						};
-						if ($('#' + t.id + 'hydroWrap').is(":hidden")){
-							$('#' + t.id + 'hydroSection').trigger('click');
-						};
-						$('#' + t.id + 'graphWrap').slideDown();
-					}else{
-						if (index > -1) {
-							t.obj.visibleLayers.splice(index, 1);
-							t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);							
-						}
-						$('#' + t.id + 'hydroHeader').html('Click map to select a HydroBASIN');
-						$('#' + t.id + 'graphWrap').slideUp();
-						t.obj.selHbDef = "";	
+// Save and Share ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+					// Save and Share Handler					
+					if (t.obj.stateSet == "yes"){
+						// // set slider values
+						// $.each(t.obj.slIdsVals,function(i,v){
+						// 	$('#' + t.id + v[0]).slider('values', v[1]);
+						// });	
+						// // checkboxes for sliders
+						// $.each(t.obj.slCbIds,function(i,v){
+						// 	$('#' + t.id + v).trigger('click');
+						// })
+						// // set radio buttons to checked state
+						// $.each(t.obj.rbIds,function(i,v){
+						// 	$('#' + t.id + v).attr('checked', true);
+						// })
+						// // checkboxes for radio buttons
+						// $.each(t.obj.rbCbIds,function(i,v){
+						// 	$('#' + t.id + v).trigger('click');
+						// })
+						// //extent
+						// var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
+						// t.map.setExtent(extent, true);
+						// t.obj.stateSet = "no";
 					}	
-				}));	
-				t.map.on("click", lang.hitch(t, function(evt) {
-					if (t.open == "yes"){
-						t.obj.pnt = evt.mapPoint;
-						var q = new Query();
-						q.geometry = t.obj.pnt;
-						t.basinFl.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
-					}	
-				}));
-				t.map.on("zoom-end", lang.hitch(t,function(e){
-					t.map.setMapCursor("pointer");
-				}));
-				t.map.on("update-end", lang.hitch(t,function(e){
-					t.map.setMapCursor("pointer");
-				}));				
+					
+				});					
 			}
 		});
     }
