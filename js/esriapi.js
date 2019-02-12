fcafe807e0e3265299e558465f46d47f73d1b3e1
Matt Silveira <msilveira@tnc.org>
2019-02-12 12:21:44 -0700

    init code build

--- js/esriapi.js
+++ js/esriapi.js
@@ -8,6 +8,9 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, Query
 
         return declare(null, {
 			esriApiFunctions: function(t){	
+				console.log('esri functions')
+
+
 				// Add dynamic map service
 				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.7});
 				t.map.addLayer(t.dynamicLayer);
@@ -18,61 +21,73 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, Query
 				t.map.on("zoom-end", function(evt){
 					t.map.setMapCursor("pointer");
 				});
+				//For Chosen options visit https://harvesthq.github.io/chosen/
+				//Single deselect only works if the first option in the select tag is blank
 
-// Dynamic layer on load ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				t.dynamicLayer.on("load", function () { 			
-					t.layersArray = t.dynamicLayer.layerInfos;
-					
-					// trigger clicks on techniques
-					$.each($('#' + t.id + 'funcWrapper input'),function(i,v){
-						if (t.obj.enhanceTech == v.value){
-							$('#' + v.id).trigger('click');	
-						}	
-					});
-					// trigger clicks on sup data radio buttons
-					$.each($('#' + t.id + 'aus-supDataWrap input'),function(i,v){
-						if (t.obj.supData == v.value){
-							$('#' + v.id).trigger('click');	
-						}	
-					});
-					// trigger initial top control clicks
-					$.each($('#' + t.id + 'aus-viewRadioWrap input'),function(i,v){
-						if (t.obj.viewRadCBCh == v.value){
-							$('#' + v.id).trigger('click');	
-						}	
+				$("#" + t.id + "habitatDropdown").chosen({allow_single_deselect:true,"disable_search": true, width:"200px"})
+					.change(function(c){
+						// var v = c.target.value;
+						// // check for a deselect
+						// if (v.length == 0){
+						// 	v = "none";
+						// }
+						// $('#' + c.target.id ).parent().next().find("span").html(v)
 					});
 
-					var extent = new Extent(144.1, -38.6, 145.7, -37.6, new SpatialReference({ wkid:4326 }))
-					if (t.obj.stateSet == "no"){
-						// t.map.setExtent(t.dynamicLayer.fullExtent.expand(1.2), true)
-						t.map.setExtent(extent, true)
-					}
-// Save and Share ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-					// Save and Share Handler					
-					if (t.obj.stateSet == "yes"){
-						// // set slider values
-						// $.each(t.obj.slIdsVals,function(i,v){
-						// 	$('#' + t.id + v[0]).slider('values', v[1]);
-						// });	
-						// // checkboxes for sliders
-						// $.each(t.obj.slCbIds,function(i,v){
-						// 	$('#' + t.id + v).trigger('click');
-						// })
-						// // set radio buttons to checked state
-						// $.each(t.obj.rbIds,function(i,v){
-						// 	$('#' + t.id + v).attr('checked', true);
-						// })
-						// // checkboxes for radio buttons
-						// $.each(t.obj.rbCbIds,function(i,v){
-						// 	$('#' + t.id + v).trigger('click');
-						// })
-						// //extent
-						// var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
-						// t.map.setExtent(extent, true);
-						// t.obj.stateSet = "no";
-					}	
+// // Dynamic layer on load ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 				t.dynamicLayer.on("load", function () { 			
+// 					t.layersArray = t.dynamicLayer.layerInfos;
 					
-				});					
+// 					// trigger clicks on techniques
+// 					$.each($('#' + t.id + 'funcWrapper input'),function(i,v){
+// 						if (t.obj.enhanceTech == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+// 					// trigger clicks on sup data radio buttons
+// 					$.each($('#' + t.id + 'aus-supDataWrap input'),function(i,v){
+// 						if (t.obj.supData == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+// 					// trigger initial top control clicks
+// 					$.each($('#' + t.id + 'aus-viewRadioWrap input'),function(i,v){
+// 						if (t.obj.viewRadCBCh == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+
+// 					var extent = new Extent(144.1, -38.6, 145.7, -37.6, new SpatialReference({ wkid:4326 }))
+// 					if (t.obj.stateSet == "no"){
+// 						// t.map.setExtent(t.dynamicLayer.fullExtent.expand(1.2), true)
+// 						t.map.setExtent(extent, true)
+// 					}
+// // Save and Share ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 					// Save and Share Handler					
+// 					if (t.obj.stateSet == "yes"){
+// 						// // set slider values
+// 						// $.each(t.obj.slIdsVals,function(i,v){
+// 						// 	$('#' + t.id + v[0]).slider('values', v[1]);
+// 						// });	
+// 						// // checkboxes for sliders
+// 						// $.each(t.obj.slCbIds,function(i,v){
+// 						// 	$('#' + t.id + v).trigger('click');
+// 						// })
+// 						// // set radio buttons to checked state
+// 						// $.each(t.obj.rbIds,function(i,v){
+// 						// 	$('#' + t.id + v).attr('checked', true);
+// 						// })
+// 						// // checkboxes for radio buttons
+// 						// $.each(t.obj.rbCbIds,function(i,v){
+// 						// 	$('#' + t.id + v).trigger('click');
+// 						// })
+// 						// //extent
+// 						// var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
+// 						// t.map.setExtent(extent, true);
+// 						// t.obj.stateSet = "no";
+// 					}	
+					
+// 				});					
 			}
 		});
     }
